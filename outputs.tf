1e1700bd05ec6744f1bca509f1dc05b0d342db56
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "api_management_identity_provider_googles_id" {
+  description = "Map of id values across all api_management_identity_provider_googles, keyed the same as var.api_management_identity_provider_googles"
+  value       = { for k, v in azurerm_api_management_identity_provider_google.api_management_identity_provider_googles : k => v.id }
+}
 output "api_management_identity_provider_googles_api_management_name" {
   description = "Map of api_management_name values across all api_management_identity_provider_googles, keyed the same as var.api_management_identity_provider_googles"
   value       = { for k, v in azurerm_api_management_identity_provider_google.api_management_identity_provider_googles : k => v.api_management_name }
