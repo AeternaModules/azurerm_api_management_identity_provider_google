b29b27b176d1fc512bb53032a55dce03fb4fd378
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:28 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,18 @@
-output "api_management_identity_provider_googles" {
-  description = "All api_management_identity_provider_google resources"
-  value       = azurerm_api_management_identity_provider_google.api_management_identity_provider_googles
-  sensitive   = true
-}
 output "api_management_identity_provider_googles_api_management_name" {
-  description = "List of api_management_name values across all api_management_identity_provider_googles"
-  value       = [for k, v in azurerm_api_management_identity_provider_google.api_management_identity_provider_googles : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_identity_provider_googles, keyed the same as var.api_management_identity_provider_googles"
+  value       = { for k, v in azurerm_api_management_identity_provider_google.api_management_identity_provider_googles : k => v.api_management_name }
 }
 output "api_management_identity_provider_googles_client_id" {
-  description = "List of client_id values across all api_management_identity_provider_googles"
-  value       = [for k, v in azurerm_api_management_identity_provider_google.api_management_identity_provider_googles : v.client_id]
+  description = "Map of client_id values across all api_management_identity_provider_googles, keyed the same as var.api_management_identity_provider_googles"
+  value       = { for k, v in azurerm_api_management_identity_provider_google.api_management_identity_provider_googles : k => v.client_id }
 }
 output "api_management_identity_provider_googles_client_secret" {
-  description = "List of client_secret values across all api_management_identity_provider_googles"
-  value       = [for k, v in azurerm_api_management_identity_provider_google.api_management_identity_provider_googles : v.client_secret]
+  description = "Map of client_secret values across all api_management_identity_provider_googles, keyed the same as var.api_management_identity_provider_googles"
+  value       = { for k, v in azurerm_api_management_identity_provider_google.api_management_identity_provider_googles : k => v.client_secret }
   sensitive   = true
 }
 output "api_management_identity_provider_googles_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_identity_provider_googles"
-  value       = [for k, v in azurerm_api_management_identity_provider_google.api_management_identity_provider_googles : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_identity_provider_googles, keyed the same as var.api_management_identity_provider_googles"
+  value       = { for k, v in azurerm_api_management_identity_provider_google.api_management_identity_provider_googles : k => v.resource_group_name }
 }
 
